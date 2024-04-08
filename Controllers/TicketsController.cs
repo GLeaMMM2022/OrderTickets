e1e7166515dd64f221fbc643b8eb96e4381eb36c
Артур Санакоев <sanakoev.2004@mail.ru>
2024-04-08 21:10:01 +0300

    создал представление для репозитория и добавления, редактирования объекта

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -1,13 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OrderTickets.Models;
 
 namespace OrderTickets.Controllers
 {
     public class TicketsController : Controller
     {
+        private readonly ITicketRepository _ticketRepository;
+
+        public TicketsController(ITicketRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+
+
 
         public IActionResult Index()
         {
+            var tickets = _ticketRepository.GetAllTickets();
             return View();
+        }
+
+        public IActionResult AddOrEdit(int id)
+        {
+            Ticket ticket;
+            if (id == 0)
+            {
+                ticket = new Ticket();
+            }
+            else
+            {
+                ticket = _ticketRepository.GetTicketById(id);
+            }
+            return View(ticket);
+        }
+
+        public IActionResult AddOrEdit(Ticket ticket) 
+        {
+            if (ModelState.IsValid)
+            {
+                if(ticket.Id == 0) 
+                {
+                    ticket.PurchaseDate = DateTime.Now;
+                    _ticketRepository.AddTicket(ticket);
+                }
+                else
+                {
+                    _ticketRepository.UpdateTicket(ticket);
+                }
+                return RedirectToAction("Index"));
+            }
+            return View(ticket);
         }
     }
 }
